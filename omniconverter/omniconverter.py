e4ef5c3902733211f51235b89b9b3278fe14e1fc
Marius Pahl <mpahl@diw.de>
2018-08-15 16:51:09 +0200

    update

--- omniconverter/omniconverter.py
+++ omniconverter/omniconverter.py
@@ -1,10 +1,13 @@
-class Converter():
+class Converter:
 
-    def array_to_string(array):
+    def __init__(self):
+        pass
+
+    def array_to_string(self, array):
         temp = []
         for i in array:
             temp.append(str(i))
         return "".join(temp)
-    
-    def string_to_array(string):
+
+    def string_to_array(self, string):
         return list(string)
